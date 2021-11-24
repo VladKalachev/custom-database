5e10981f29416882e64a6d2ecceb7421f1440839
Corvax <vlvete1990@yandex.ru>
2021-11-24 23:55:08 +0300

    chore: added test

--- Table.test.js
+++ Table.test.js
@@ -5,13 +5,27 @@ const Table = require('./Table')
 const TableDoesNotExistsError = require('./errors/TableDoesNotExistsError')
 
 describe('#readData', () => {
-  describe('WIth nonexisting table name', () => {
+  describe('With nonexisting table name', () => {
      beforeEach(() => mock({ data: {} }))
      afterEach(mock.restore)
 
      test('It throws TableDoesNotExistsError', async () => {
       const table = new Table('table')
-      await expect(table.readData()).rejects.toThrow(TableDoesNotExistsError)
+      expect(await table.readData()).rejects.toThrow(TableDoesNotExistsError)
      })
+  })
+
+  describe('With an existing table name', () => {
+    const data = [
+      { a: 1, b: 2 },
+      { a: 3, b: 4 }
+    ]
+    beforeEach(() => mock({ data: { "table.json": JSON.stringify(data) } }))
+    afterEach(mock.restore)
+
+    test('It gets all the data in the table', async () => {
+     const table = new Table("table")
+     expect(await table.readData()).toEqual(data)
+    })
   })
 })
