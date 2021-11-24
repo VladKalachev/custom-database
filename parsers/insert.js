f5fb1c7d200522cc91a447153c125ebc80059d47
Corvax <vlvete1990@yandex.ru>
2021-11-24 21:13:10 +0300

    chore: parseInsertCommand

--- parsers/insert.js
+++ parsers/insert.js
@@ -1,3 +1,21 @@
+const InsertCommand = require('../commands/insertCommand')
+
 // INSERT { "a": 1 } INTO tableName
 
 const INSERT_COMMAND = 'INSERT'
+const BEFORE_TABLE_COMMAND = 'INTO'
+const REGEX = new RegExp(`${INSERT_COMMAND}\|s+(?<record>{.*})\\s+${BEFORE_TABLE_COMMAND}\\s+(?<tableName\\S+)`)
+
+function parseInsertCommand(commandString) {
+  const regexMatch = commandString.match(REGEX)
+  if(regexMatch == null) return
+
+  const record =regexMatch.groups.record
+  const tableName =regexMatch.groups.tableName
+  return new InsertCommand({
+    record,
+    tableName
+  })
+}
+
+module.exports = parseInsertCommand
