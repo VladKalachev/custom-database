ece8836f093cea3447a1f13566ec608c808d31d8
Corvax <vlvete1990@yandex.ru>
2021-11-26 16:58:17 +0300

    chore: added test for insertRecord

--- Table.test.js
+++ Table.test.js
@@ -1,6 +1,6 @@
 const { afterEach, beforeEach, expect } = require('@jest/globals')
 const mock  = require('mock-fs')
-
+const fs = require('fs')
 const Table = require('./Table')
 const TableDoesNotExistsError = require('./errors/TableDoesNotExistError')
 
@@ -28,4 +28,46 @@ describe('#readData', () => {
      expect(await table.readData()).toIncludeSameMembers(data)
     })
   })
+})
+
+describe("#insertRecord", () => {
+  describe('With nonexisting table name', () => {
+       beforeEach(() => mock({ data: {} }))
+       afterEach(mock.restore)
+  
+      test('It creates the table and adds te recird', async () => {
+        const table = new Table('table')
+        const recirdToInsert = { a: 1, b: 2 }
+        const { _id, ...newRecirdAttributes } = await table.insertRecord(recirdToInsert)
+        
+        expect(
+          JSON.parse(fs.readFileSync("data/table.json"))
+        ).toIncludeSameMembers([{ _id, ...newRecirdAttributes }])
+
+        expect(_id).toBeDefined()
+        expect(newRecirdAttributes).toEqual(recirdToInsert)
+       })
+    })
+
+  describe('With an existing table', () => {
+    const data = [
+      { a: 1, b: 2 },
+      { a: 3, b: 4 }
+    ]
+    beforeEach(() => mock({ data: { "table.json": JSON.stringify(data) } }))
+    afterEach(mock.restore)
+ 
+    test('It adds the record', async () => {
+       const table = new Table('table')
+       const recirdToInsert = { a: 5, b: 6 }
+       const { _id, ...newRecirdAttributes } = await table.insertRecord(recirdToInsert)
+       
+       expect(
+         JSON.parse(fs.readFileSync("data/table.json"))
+       ).toIncludeSameMembers([...data, { _id, ...newRecirdAttributes }])
+
+       expect(_id).toBeDefined()
+       expect(newRecirdAttributes).toEqual(recirdToInsert)
+      })
+   })
 })
