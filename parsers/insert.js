b162152dc392d9f9fa17c3a4039f0bdfac248b31
Corvax <vlvete1990@yandex.ru>
2021-11-26 18:15:41 +0300

    chore: added where command

--- parsers/insert.js
+++ parsers/insert.js
@@ -1,14 +1,15 @@
-const InsertCommand = require('../commands/InsertCommand')
+const InsertCommand = require("../commands/InsertCommand")
+const safeParseJSON = require("../utils/safeParseJSON")
 
-// INSERT { "a": 1 } INTO tableName
-
-const INSERT_COMMAND = 'INSERT'
-const BEFORE_TABLE_COMMAND = 'INTO'
-const REGEX = new RegExp(`${INSERT_COMMAND}\\s+(?<record>{.*})\\s+${BEFORE_TABLE_COMMAND}\\s+(?<tableName>\\S+)`)
+const INSERT_COMMAND = "INSERT"
+const BEFORE_TABLE_COMMAND = "INTO"
+const REGEX = new RegExp(
+  `${INSERT_COMMAND}\\s+(?<record>{.*})\\s+${BEFORE_TABLE_COMMAND}\\s+(?<tableName>\\S+)`
+)
 
 function parseInsertCommand(commandString) {
   const regexMatch = commandString.match(REGEX)
-  if(regexMatch == null) return
+  if (regexMatch == null) return
 
   const record = safeParseJSON(regexMatch.groups.record)
   if (record == null) return
@@ -16,17 +17,10 @@ function parseInsertCommand(commandString) {
   const tableName = regexMatch.groups.tableName
 
   return new InsertCommand({
+    tableName,
     record,
-    tableName
   })
 }
 
-function safeParseJSON(string) {
-  try {
-    return JSON.parse(string)
-  } catch (error) {
-    return 
-  }
-}
-
 module.exports = parseInsertCommand
+
